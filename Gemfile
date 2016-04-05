5d0d534f844e1fdd63dcba7fa78d294819e5dc34
yasunakacho <yasu1987@me.com>
2016-04-05 10:43:11 +0900

    task5-6 finished

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'toastr-rails'
 gem 'omniauth'
 gem 'omniauth-facebook'
 
+gem 'paperclip'
+
 group :development, :test do
   gem 'byebug'
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,10 @@
 class Room < ActiveRecord::Base
   belongs_to :user
+  has_many :photos
 
   validates :home_type, presence: true
   validates :room_type, presence: true
-  validates :acoommodate, presence: true
+  validates :accommodate, presence: true
   validates :bed_room, presence: true
   validates :bath_room, presence: true
   validates :listing_name, presence: true, length: {maximum: 50}
